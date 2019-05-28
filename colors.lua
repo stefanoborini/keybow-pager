effb3f20a400149365ffc6aaaf40433975dbc4fc
Stefano Borini <stefano.borini@gmail.com>
2019-05-29 00:10:00 +0100

    Added colors

---
+++ colors.lua
@@ -0,0 +1,17 @@
+color = {}
+
+color.DARK_RED = { 127, 0, 0 }
+color.RED = { 127, 0, 0 }
+color.DARK_BLUE = { 0, 0, 127 }
+color.BLUE = { 0, 0, 255 }
+color.DARK_GREEN = { 0, 127, 0 }
+color.GREEN = { 0, 255, 0 }
+color.DARK_YELLOW = { 127, 127, 0 }
+color.YELLOW = { 255, 255, 0 }
+color.DARK_CYAN = { 0, 127, 127 }
+color.CYAN = { 0, 255, 255 }
+color.DARK_MAGENTA = { 127, 0, 127 }
+color.MAGENTA = { 255, 0, 255 }
+color.WHITE = { 255, 255, 255 }
+color.NONE = { 0, 0, 0 }
+
